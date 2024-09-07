145068edbcb35580926db6bbc4c0bc5c01969ea1
Moncef SAMAI <Moncef.samai.rol2@gmail.com>
2024-09-07 16:43:00 -0500

    fetching module created

---
+++ DataCollection/fetchTest.py
@@ -0,0 +1,21 @@
+import os
+from session import login_to_garmin, username, password
+
+
+def fetch_act(session): 
+    activity_url = os.getenv('activityUrl')
+    activityResponse = session.get(activity_url)
+
+    data = activityResponse.json()
+
+    # expect 2D array 
+    print(data.type) 
+
+
+    return data['activities']
+
+
+
+#session1 = login_to_garmin(username, password)
+#data = fetch_act(session1); 
+
