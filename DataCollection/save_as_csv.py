1415f36b9c69810e683ad7f5444aa8f7e4a6478a
Moncef SAMAI <Moncef.samai.rol2@gmail.com>
2024-09-07 16:44:28 -0500

    Create and save csv module

---
+++ DataCollection/save_as_csv.py
@@ -0,0 +1,22 @@
+import csv 
+from fetchTest import fetchTest 
+
+def save_as_csv(activities, filename='testDataset.csv'): 
+    header = ["Activity ID", "Activity Type", "Distance", "Time"]
+
+    with open(filename, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file) 
+        # append the head 
+        writer.writerow(header) 
+
+        for activity in activities: 
+            row = [
+                activity["Activity ID"], 
+                activity["Activity Type"], 
+                activity["Distance"], 
+                activity["Time"]
+            ]
+
+            writer.writerow(row)
+
+
